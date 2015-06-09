55c5b5f1b20d42953f6e9ed7e65181d62ba2d448
Ian Gerrebrands <gerrebrands.i@gmail.com>
2015-06-09 12:31:56 +0200

    #MainView added getInputEmail, getInputPassword & showWarningToast #MainActivity edited onClick

--- app/src/main/java/nl/mondriaanict/ao/rooster/control/MainActivity.java
+++ app/src/main/java/nl/mondriaanict/ao/rooster/control/MainActivity.java
@@ -5,18 +5,26 @@ import android.os.Bundle;
 import android.view.View;
 
 import nl.mondriaanict.ao.rooster.R;
+import nl.mondriaanict.ao.rooster.view.MainView;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
+
+    MainView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.view = new MainView(this);
     }
 
     @Override
     public void onClick(View v) {
-
+        if(view.getInputEmail().length() <1 || view.getInputPassword().length() < 1){
+            view.showWarningToast("Please fill in all fields!");
+            return;
+        }
+        // TODO json script thingies
     }
 }
